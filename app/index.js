990ac65a35f23387c7c042a70973ccabf8400d01
Ben <ncor89b@gmail.com>
2023-05-23 21:39:44 +0200

    style(index.js): Adding new TODOS comments

--- app/index.js
+++ app/index.js
@@ -1,6 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
 
+// TODO
+//: RESTART HERE
+//: FIND WEATHER API KEY
+
 export default function Home() {
   return (
     <View>
