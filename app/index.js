d53ba476134a01f67fef5647c6ccf37cf7d086cc
Ben <ncor89b@gmail.com>
2023-05-24 21:31:42 +0200

    style(index.js): Adding new comments for Process

--- app/index.js
+++ app/index.js
@@ -1,9 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-// TODO
+// TODO (1)
 //: RESTART HERE
 //: FIND WEATHER API KEY
+
+// TODO (2)
+//: add nativewind + tailwindcss
+//: https://www.nativewind.dev/quick-starts/expo
 
 export default function Home() {
   return (
