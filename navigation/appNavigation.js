17bb5cd4b70d753ef56c3b8a018e379a4be231ef
Ben <ncor89b@gmail.com>
2023-05-24 21:34:14 +0200

    build(appNavigation.js): Start implementation of Navigation app

---
+++ navigation/appNavigation.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+// TODO :
+//: Create Screens and Import in this file
+
+export default function AppNavigation() {
+  return <NavigationContainer></NavigationContainer>;
+}
