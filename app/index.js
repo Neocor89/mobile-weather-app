b904888c9c345d6862e85628b9ae4e0374a7b0e4
Ben <ncor89b@gmail.com>
2023-05-28 20:52:24 +0200

    build(weather.js): Creating background image app presentation

--- app/index.js
+++ app/index.js
@@ -1,14 +1,25 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 
-// TODO (1)
-//: RESTART HERE
-//: FIND WEATHER API KEY
+// TODO
+//+ RESTART HERE
+//? FIND WEATHER API KEY
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.image}>
+      <Image
+        source={require("../assets/images/cloud-bg.png")}
+        style={{ display: "flex", flex: 1 }}
+        resizeMode="cover"
+        blurRadius={9}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+  },
+});
