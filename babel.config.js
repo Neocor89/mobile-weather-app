4ad1b3d5a7ebb793ef743a30a197a869b976a89a
Ben <ncor89b@gmail.com>
2023-05-28 20:55:21 +0200

    build: Deleting nativewind and tailwindcss libraries not work

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,6 @@ module.exports = function (api) {
     plugins: [
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
-      "react-native-dotenv",
       require.resolve("expo-router/babel"),
     ],
   };
