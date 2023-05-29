d47f0cdb57757e14de0e063238b6776b2b86c1ca
Ben <ncor89b@gmail.com>
2023-05-29 15:37:29 +0200

    build(weather.js): Define endpoints for using API

--- api/weather.js
+++ api/weather.js
@@ -1,12 +1,34 @@
-export function endPointURL() {
-  `http://dataservice.accuweather.com/`;
-}
+import axios from "axios";
+import { apiKey } from "../constants";
 
-const locationEndPointURL = (params) =>
-  `http://dataservice.accuweather.com/${params}/v1/`;
+//+ RESTART HERE
 
-const searchEndPointURL = (params) =>
-  `http://dataservice.accuweather.com/${params}/v1/`;
+const forecastEndpoint = (params) =>
+  `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${params.cities}`;
+const locationsEndpoint = (params) =>
+  `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${params.cities}&details=true&offset=5`;
+const apiCall = async (endpoint) => {
+  const options = {
+    method: "GET",
+    url: endpoint,
+  };
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    console.log("error: ", error);
+    return {};
+  }
+};
 
-const searchURL = (params, paramsCities, apiKey) =>
-  `https://dataservice.accuweather.com/${params}/v1/${paramsCities}/search?apikey=${apiKey}`;
+// http://dataservice.accuweather.com/locations/v1/${params.cities}/search?apikey=${apiKey}=${params.cities}&language=us-US&details=true&offset=1
+
+// http://dataservice.accuweather.com/locations/v1/${params.countries}/US
+
+// http://dataservice.accuweather.com/locations/v1/${params.cities}/autocomplete
+
+// http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${params.cities}&details=true&offset=5
+
+// http://dataservice.accuweather.com/forecasts/v1/daily/5day/{params}
+// OR
+// http://dataservice.accuweather.com/forecasts/v1/daily/5day/{params.cities}
