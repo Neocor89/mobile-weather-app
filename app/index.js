5273763c386830bf0f450307b8f66ce2ad459bf6
Ben <ncor89b@gmail.com>
2023-06-04 00:06:17 +0200

    feat: Adding and implementing Stats and Weather provisional static sections

--- app/index.js
+++ app/index.js
@@ -2,23 +2,32 @@ import { StyleSheet, Image, TouchableOpacity, Text } from "react-native";
 import React, { useState } from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import {
+  ImageForecast,
+  ImageStatsWind,
   NewTouchableOpacityLoc,
   SafeAreaViewStyled,
   TextCities,
   TextCountry,
+  TextDegree,
   TextForecast,
   TextInputStyled,
+  TextWeather,
+  TextWind,
   TouchableOpacityLocStyled,
   TouchableOpacityStyled,
+  ViewDegreeContainer,
   ViewForecast,
+  ViewForecastImage,
   ViewInStyled,
   ViewLocation,
+  ViewStatsContainer,
+  ViewStatsContent,
   ViewStyled,
 } from "../theme";
 
 // TODO
 
-//+ CONTINUING LOCATIONS DISPLAY
+//+ CONTINUING LOCATIONS TEMPERATURES ICONS STYLING
 
 export default function Home() {
   const [showSearch, setShowSearch] = useState(false);
@@ -94,6 +103,43 @@ export default function Home() {
             Bogota,
             <TextCountry>Colombia</TextCountry>
           </TextForecast>
+
+          {/* WEATHER IMAGE SECTION */}
+          <ViewForecastImage>
+            <ImageForecast
+              source={require("../assets/images/3D-lighting-cloud.png")}
+            ></ImageForecast>
+          </ViewForecastImage>
+
+          {/* DEGREE DISPLAYING SECTION */}
+          <ViewDegreeContainer>
+            <TextDegree>23&#176;</TextDegree>
+            <TextWeather>Storm</TextWeather>
+          </ViewDegreeContainer>
+
+          {/* WEATHER STATS SECTION */}
+          <ViewStatsContainer>
+            <ViewStatsContent>
+              <ImageStatsWind
+                source={require("../assets/images/wind-info.png")}
+              ></ImageStatsWind>
+              <TextWind>28km</TextWind>
+            </ViewStatsContent>
+
+            <ViewStatsContent>
+              <ImageStatsWind
+                source={require("../assets/images/wind-info.png")}
+              ></ImageStatsWind>
+              <TextWind>28km</TextWind>
+            </ViewStatsContent>
+
+            <ViewStatsContent>
+              <ImageStatsWind
+                source={require("../assets/images/wind-info.png")}
+              ></ImageStatsWind>
+              <TextWind>28km</TextWind>
+            </ViewStatsContent>
+          </ViewStatsContainer>
         </ViewForecast>
       </SafeAreaViewStyled>
     </>
