94390cb9a879ad19414003736dbcbfeefc50f91f
Ben <ncor89b@gmail.com>
2023-06-11 21:23:41 +0200

    build(weather.js): Adding new API for collecting data weather

--- api/weather.js
+++ api/weather.js
@@ -1,13 +1,26 @@
 import axios from "axios";
-import { apiKey } from "../constants";
 
 //+ RESTART HERE
-//: TESTING CALL PARAMS WORK FOR API
+// TODO
+//: CREATE ENV FILE AND FIND PROCESS FOR APIKEY
+
+const apiKey = "";
 
 const forecastEndpoint = (params) =>
-  `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${params.cities}`;
+  //: NEW API
+  `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&days=${params.days}&aqi=no&alerts=no`;
+
+// AFTER http://dataservice.accuweather.com/locations/v1/${params.cities}/autocomplete
+// BEFORE `http://dataservice.accuweather.com/forecasts/v1/daily/${params.days}day/${params.cities}`
+
 const locationsEndpoint = (params) =>
-  `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${params.cities}&details=true&offset=5`;
+  `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${params.cities}`;
+
+// http://dataservice.accuweather.com/locations/v1/${params.cities}/search
+
+// NEW /locations/v1/${params.cities}/search?apikey=${apiKey}&q=${params.cities}
+// BEFORE http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${params.cities}
+
 const apiCall = async (endpoint) => {
   const options = {
     method: "GET",
@@ -20,6 +33,13 @@ const apiCall = async (endpoint) => {
     console.log("error: ", error);
     return {};
   }
+};
+
+export const fetchWeatherForecast = (params) => {
+  return apiCall(forecastEndpoint(params));
+};
+export const fetchLocations = (params) => {
+  return apiCall(locationsEndpoint(params));
 };
 
 // http://dataservice.accuweather.com/locations/v1/${params.cities}/search?apikey=${apiKey}=${params.cities}&language=us-US&details=true&offset=1
