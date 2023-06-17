f166297f9877f0ef4438203a06d0c6c76f514ed8
Ben <ncor89b@gmail.com>
2023-06-17 22:32:43 +0200

    feat: Creating constants folder and imports my images for displaying in App

---
+++ constants/index.js
@@ -0,0 +1,20 @@
+export const apiKey = process.env.API_KEY;
+
+export const weatherImages = {
+  "Partly cloudy": require("../assets/images/"),
+  "Moderate rain": require("../assets/images/"),
+  "Patchy rain possible": require("../assets/images/"),
+  "Sunny": require("./weather/sun.png"),
+  "Light rain": require("../assets/images/3D-lighting-cloud.png"),
+  "Clear": require("../assets/images/"),
+  "Overcast": require("../assets/images/"),
+  "Cloudy": require("../assets/images/"),
+  "Light rain": require("../assets/images/"),
+  "Moderate rain at times": require("../assets/images/"),
+  "Heavy rain": require("../assets/images/"),
+  "Heavy rain at times": require("../assets/images/"),
+  "Moderate or heavy freezing rain": require("../assets/images/"),
+  "Moderate or heavy rain shower": require("../assets/images/"),
+  "Moderate or heavy rain with thunder": require("../assets/images/"),
+  "other": require("../assets/images/"),
+};
